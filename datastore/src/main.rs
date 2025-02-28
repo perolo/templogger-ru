fbc1d8eeb8ad87b5145ba574adb405a5c702236d
Per Olofsson <per.e.olofsson@gmail.com>
2025-02-28 14:57:40 +0100

    Some cleanup

--- datastore/src/main.rs
+++ datastore/src/main.rs
@@ -1,65 +1,45 @@
-use std::net::UdpSocket;
-//use surrealdb::{Surreal, opt::auth::Root};
-//use surrealdb::engine::local::Mem;
-//use surrealdb::engine::remote::ws::Ws;
-use postcard::from_bytes;
-//use serde::{Serialize, Deserialize};
-use tokio;
-use protocol::Temperature;
-use influxdb_rs::Client;
-use influxdb_rs::Point;
-use influxdb_rs::Value;
-//use influxdb_rs::Precision;
-use url::Url;
 use chrono::prelude::*;
+use influxdb_rs::{Client, Point, Value};
+use postcard::from_bytes;
+use protocol::Temperature;
+use std::net::UdpSocket;
+use tokio;
+use url::Url;
 
 #[tokio::main]
 async fn main() {
-    let socket = UdpSocket::bind("0.0.0.0:5000").expect("Could not bind to UDP socket");
-//    let socket = UdpSocket::bind("127.0.0.1:5000").expect("Could not bind to UDP socket");
-//    let socket = UdpSocket::bind("127.0.0.1:5000").unwrap();
+    let socket: UdpSocket = UdpSocket::bind("0.0.0.0:5000").expect("Could not bind to UDP socket");
 
-//    let db = Surreal::new::<Ws>("localhost:8000").await.expect("Failed to initialize SurrealDB");
-//    db.signin(Root { username: "root", password: "root" }).await.expect("Failed to sign in");
-//    db.use_ns("temperature").use_db("data").await.expect("Failed to select namespace and database");
-    
-    let client = Client::new(Url::parse("http://localhost:8086").unwrap(), 
-                        "temperature", 
-                        "root", 
-                        "tHWZxGg3_wdzqLUmikkOwXZtylFnD8l1u9sOGbeZuWYMiThBck6LO9qvexMwNrL4iOnxQQp8cVZG_dSkxGmUYw==")
-                        .await.unwrap();
+    let client: Client = Client::new(
+        Url::parse("http://localhost:8086").unwrap(),
+        "temperature",
+        "root",
+        "tHWZxGg3_wdzqLUmikkOwXZtylFnD8l1u9sOGbeZuWYMiThBck6LO9qvexMwNrL4iOnxQQp8cVZG_dSkxGmUYw==",
+    )
+    .await
+    .unwrap();
 
-    let mut buf = [0u8; 1024];
-    
+    let mut buf: [u8; 1024] = [0u8; 1024];
+
     loop {
-        let (size, _src) = socket.recv_from(&mut buf).expect("Failed to receive UDP packet");
-        
+        let (size, _src) = socket
+            .recv_from(&mut buf)
+            .expect("Failed to receive UDP packet");
+
         if let Ok(data) = from_bytes::<Temperature>(&buf[..size]) {
             println!("Received: {:?}", data);
 
-            let now = Utc::now();
-            let ii:i32 = data.id as i32;
-            println!("ii: {:?}", ii);
+            let now: DateTime<Utc> = Utc::now();
+            let point: Point<'_> = Point::new("temperature")
+                .add_tag("sensor_id", Value::Integer(data.id.into()))
+                .add_field("temperature", Value::Float(data.temperature.into()))
+                .add_timestamp(now.timestamp());
 
-            let point = Point::new("temperature")
-            .add_tag("sensor_id", Value::Integer(ii.into()))
-            .add_field("temperature", Value::Float(data.temperature.into()))
-            .add_timestamp(now.timestamp());
-
-        let result = client.write_point(point, None, None).await;
-        if let Err(e) = result {
-            eprintln!("Failed to write to InfluxDB: {:?}", e);
-        } else {
-            println!("Data written to InfluxDB");
-        }   
-            /*
-            let _: Option<Temperature> = db
-                .create("temperature")
-                .content(data)
-                .await
-                .expect("Failed to insert data into SurrealDB");
-            */
-            
+            let result: Result<(), influxdb_rs::Error> =
+                client.write_point(point, None, None).await;
+            if let Err(e) = result {
+                eprintln!("Failed to write to InfluxDB: {:?}", e);
+            }
         } else {
             eprintln!("Failed to deserialize received data");
         }
--- logger/src/main.rs
+++ logger/src/main.rs
@@ -2,18 +2,15 @@ mod ds18b20;
 mod w1_errors;
 use java_properties::read;
 
-use std::{fs::File, io::BufReader, thread, time, net::UdpSocket};
 use postcard::to_stdvec;
 use protocol::Temperature;
-
-
+use std::{fs::File, io::BufReader, net::UdpSocket, thread, time};
 
 fn main() {
-
     let file_name = "/home/pero/src/rust/pero/templogger-ru/logger/src/rutemplogger.properties"; //env::current_dir().unwrap();
 
     // Reading
-    let  f = File::open(&file_name).unwrap();
+    let f = File::open(&file_name).unwrap();
     let map2 = read(BufReader::new(f)).unwrap();
     println!("map{} ", map2["sensornames"]);
 
@@ -21,8 +18,8 @@ fn main() {
     let sensornames: Vec<&str> = map2["sensornames"].split(',').collect();
 
     if expectedsensors.len() != sensornames.len() {
-        println!("expected vectors of equal length");  
-        panic!("Panicking...");          
+        println!("expected vectors of equal length");
+        panic!("Panicking...");
     }
 
     let target_ip = "192.168.50.60:5000"; // Change this to your target IP and port
@@ -37,41 +34,41 @@ fn main() {
         if expectedsensors.iter().any(|e| e == &s) {
             println!("expectedsensor found:{} name: {}", s, sensornames[i]);
         } else {
-            println!("expectedsensor not found:{} ", s);  
-            panic!("Panicking...");          
+            println!("expectedsensor not found:{} ", s);
+            panic!("Panicking...");
         }
     }
 
     let sleep_delay = time::Duration::from_secs(30);
     let mut now = time::Instant::now();
     loop {
-        
         for sensor in &sensors.w1_id {
             let s = String::from(sensor);
-            //let ss = String::from(sensor);
             let temp_res = sensors.read_temp(s.clone());
             match temp_res {
-                Err(_error) => println!("Error reading {sensor}" ),
-                Ok(temp) =>  {
-                        let t = temp.to_celsius();
-                        let i = expectedsensors.iter().position(|&r| r == &s).unwrap();
-                        println!("{} no {}: {:.1} C", sensor, &i, &t);
-                        let ii: u8 = i as u8;
-                        let data = Temperature {  id: ii ,temperature:t};
-                        //let data = CpuTemperature { temperature: temp };
-                        match to_stdvec(&data) {
-                            Ok(serialized) => {
-                                let _ = socket.send_to(&serialized, target_ip);
-                            }
-                            Err(e) => eprintln!("Serialization error: {}", e),
+                Err(_error) => println!("Error reading {sensor}"),
+                Ok(temp) => {
+                    let t = temp.to_celsius();
+                    let i = expectedsensors.iter().position(|&r| r == &s).unwrap();
+                    println!("{} no {}: {:.1} C", sensor, &i, &t);
+                    let ii: u8 = i as u8;
+                    let data = Temperature {
+                        id: ii,
+                        temperature: t,
+                    };
+                    match to_stdvec(&data) {
+                        Ok(serialized) => {
+                            let _ = socket.send_to(&serialized, target_ip);
                         }
-                },
+                        Err(e) => eprintln!("Serialization error: {}", e),
+                    }
+                }
             }
         }
         let read_done = time::Instant::now();
         now = now.checked_add(sleep_delay).unwrap();
         let dur = now - read_done;
-        println!("Sleep {:#?} s",  dur );
+        println!("Sleep {:#?} s", dur);
         thread::sleep(dur);
     }
 }
